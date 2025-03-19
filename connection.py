20058a8b47e5add280fc506a0c419f483268fc2f
VBF997 <veronica.betancourt@mail.escuelaing.edu.co>
2025-03-19 16:36:57 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -5,6 +5,6 @@ from pymongo.server_api import ServerApi
 def connect_to_mongodb(db_name, collection_name):
     uri = "mongodb+srv://VVBF997:Hefe4976B#@hospitalinformationsyst.rqpcw.mongodb.net/?retryWrites=true&w=majority&appName=HospitalInformationSystem"
     client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client[proyectoHIS]
+    db = client[SamplePatientService]
     collection = db[patients]
     return collection
