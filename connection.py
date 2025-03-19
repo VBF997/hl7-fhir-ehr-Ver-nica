e9e6d4328a8d79567f469bbbdfc19d79c3cf4bfd
VBF997 <veronica.betancourt@mail.escuelaing.edu.co>
2025-03-19 15:50:34 -0500

    guaradar el db name y collection name

--- connection.py
+++ connection.py
@@ -5,6 +5,6 @@ from pymongo.server_api import ServerApi
 def connect_to_mongodb(db_name, collection_name):
     uri = "mongodb+srv://VVBF997:Hefe4976B#@hospitalinformationsyst.rqpcw.mongodb.net/?retryWrites=true&w=majority&appName=HospitalInformationSystem"
     client = MongoClient(uri, server_api=ServerApi('1'))
-    db = client[db_name]
-    collection = db[collection_name]
+    db = client[proyectoHIS]
+    collection = db[patients]
     return collection
